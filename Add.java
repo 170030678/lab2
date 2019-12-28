f44ea31cff81f08deb26a8ed87af79ae14c6daa6
170030678 <36721562+170030678@users.noreply.github.com>
2019-12-28 14:39:56 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Add{
+
+    public static void main(String[] args) {
+        
+        int frist =10;
+        int second=20;
+        int sum=frist+second;
+        
+      
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
